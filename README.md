7f4719d5587c3053132379bac8f39b70ccdb57b7
Dipal018 <dipal042@gmail.com>
2016-03-03 16:38:46 -0800

    Update and rename README.md to Take_a_break.py

--- README.md
+++
@@ -1,2 +0,0 @@
-# Python-Projects
-This will include all the python projects that i have learned.
---
+++ Take_a_break.py
@@ -0,0 +1,13 @@
+import webbrowser
+import time
+break_count=0
+
+print ("This program Started on: " + time.ctime())
+while break_count < 3:
+    time.sleep(2*60*60)
+    webbrowser.open("https://www.youtube.com/watch?v=4nd01DwuHwE")
+    break_count+=1
+
+Output: It will open a web-browser and play video 3 times after every 2 hours.
+
+
